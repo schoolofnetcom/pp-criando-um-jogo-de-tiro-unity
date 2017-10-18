73d926edf1752b90711186a32449aeed41a18228
Victor Lima <jvlpark3@outlook.com>
2017-10-18 14:30:27 -0200

    Add files via upload

---
+++ Scripts/Personagem/FPSCamera.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCamera : MonoBehaviour {
+
+	// Vars de Eixos
+	public enum EixosDeRotacao {MouseX, MouseY};
+	public EixosDeRotacao eixos = EixosDeRotacao.MouseY;
+
+	// Vars de Sens
+
+	private float sensXSet = 1.5f;
+	private float sensYSet = 1.5f;
+	private float sensX = 1.5f;
+	private float sensY = 1.5f;
+
+	private float sensMouse = 1.5f;
+
+	// Vars de Angulos
+
+	private float rotacaoX, rotacaoY;
+
+	// Limites
+
+	private float maximumX = 360f;
+	private float minimumX = -360f;
+	private float maximumY = 60f;
+	private float minimumY = -60f;
+
+	// Rotacao
+	private Quaternion rotacao;
+
+	// Use this for initialization
+	void Start () {
+		rotacao = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ManipulaCamera ();	
+	}
+
+	float limitarAngulos(float angulo, float min, float max){
+
+		if(angulo < -360f){
+			angulo += 360f;
+		}
+
+		if(angulo > 360f){
+			angulo -= 360f;
+		}
+
+		return Mathf.Clamp (angulo,min,max);
+	
+	}
+
+	void ManipulaCamera(){
+		if (sensXSet != sensMouse || sensYSet != sensMouse) {
+			sensXSet = sensYSet = sensMouse;
+		}
+		sensX = sensXSet;
+		sensY = sensYSet;
+
+		if(eixos == EixosDeRotacao.MouseX){
+			rotacaoX += Input.GetAxis ("Mouse X") * sensX;
+			rotacaoX = limitarAngulos (rotacaoX,minimumX,maximumX);
+			Quaternion xQuaternion = Quaternion.AngleAxis (rotacaoX,Vector3.up);
+			transform.localRotation = xQuaternion * rotacao;
+		}
+
+		if (eixos == EixosDeRotacao.MouseY) {
+			rotacaoY += Input.GetAxis ("Mouse Y") * sensY;
+			rotacaoY = limitarAngulos (rotacaoY, minimumY, maximumY);
+			Quaternion yQuaternion = Quaternion.AngleAxis (-rotacaoY, Vector3.right);
+			transform.localRotation = yQuaternion * rotacao;
+		}
+			
+	}
+}
---
+++ Scripts/Personagem/FPSPersoangem.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSPersoangem : MonoBehaviour {
+
+	// Vars de Transform
+
+	private Transform fpsView;
+	private Transform fpsCamera;
+	private Vector3 fpsRotation = Vector3.zero;
+
+	// Vars de Velocidade
+	public float velocidadeAndando = 6.7f;
+	public float velocidadeCorrendo = 10.0f;
+	public float pulo = 8f;
+	public float gravidade = 20f;
+
+	private float velocidade;
+
+	// Teclado
+
+	private float inputX, inputY;
+	private float inputXSet, inputYSet;
+	private float fator;
+	private bool limitarVelocidadeDiagonal = true;
+	private float antiToque = 0.75f;
+
+	// Logicas
+
+	private bool noChao, seMovendo;
+
+	// Controle
+
+	private CharacterController charController;
+	private Vector3 direcaoMovimento = Vector3.zero;
+
+	// Pulo, Corrida e Agachar
+
+	public LayerMask camadaDoChao;
+	private float rayDistance;
+	private float alturaPadrao;
+	private Vector3 camPosPadrao;
+	private float camAltura;
+	private bool estaAgachado;
+	private float velocidadeAgachado = 3.15f;
+
+	// Use this for initialization
+	void Start () {
+		fpsView = transform.Find ("FPS Visao").transform;
+		charController = GetComponent<CharacterController> ();
+		velocidade = velocidadeAndando;
+		seMovendo = false;
+
+		rayDistance = charController.height * 0.5f + charController.radius;
+		alturaPadrao = charController.height;
+		camPosPadrao = fpsView.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movimento ();	
+	}
+
+	void Movimento(){
+		// Detectando movimento no Eixo Y
+		if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)){
+			if (Input.GetKey (KeyCode.W)) {
+				inputYSet = 1f;	
+			} else {
+				inputYSet = -1f;
+			}
+		}else{
+			inputYSet = 0f;
+		}
+
+		// Detectando movimento no Eixo X
+
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.D)) {
+			if(Input.GetKey (KeyCode.D)){
+				inputXSet = 1f;
+			}else{
+				inputXSet = -1f;
+			}
+		} else {
+			inputXSet = 0f;
+		}
+
+		inputX = Mathf.Lerp (inputX, inputXSet, Time.deltaTime * 20f);
+		inputY = Mathf.Lerp (inputY, inputYSet, Time.deltaTime *20f);
+		fator = Mathf.Lerp (fator, (inputYSet != 0 && inputXSet != 0 && limitarVelocidadeDiagonal) ? 0.75f : 1.0f, Time.deltaTime * 20f);
+
+		fpsRotation = Vector3.Lerp (fpsRotation, Vector3.zero, Time.deltaTime * 5f);
+		fpsView.localEulerAngles = fpsRotation;
+
+		if(noChao){
+			AgachaECorre ();
+			direcaoMovimento = new Vector3 (inputX * fator,-antiToque,inputY * fator);
+			direcaoMovimento = transform.TransformDirection (direcaoMovimento) * velocidade;
+		}
+		direcaoMovimento.y -= gravidade * Time.deltaTime;
+
+		noChao = (charController.Move (direcaoMovimento * Time.deltaTime) & CollisionFlags.Below) != 0;
+		seMovendo = charController.velocity.magnitude > 0.15f;
+	}
+
+
+	void AgachaECorre(){
+		if(Input.GetKeyDown(KeyCode.C)){
+			if (!estaAgachado) {
+				estaAgachado = true;
+			} else {
+				if(podeSeLevantar()){
+					estaAgachado = false;
+				}
+			}
+		}
+			
+		StopCoroutine (MoveCamera());
+		StartCoroutine (MoveCamera ());
+
+		if (estaAgachado) {
+			velocidade = velocidadeAgachado;
+		} else {
+			if(Input.GetKeyDown(KeyCode.LeftShift)){
+				velocidade = velocidadeCorrendo;
+			}else{
+				velocidade = velocidadeAndando;
+			}
+		}
+	}
+
+	bool podeSeLevantar(){
+		Ray rayTopo = new Ray (transform.position,transform.up);
+		RaycastHit rayTopoHit;
+
+		if(Physics.SphereCast(rayTopo, charController.radius + 0.05f, out rayTopoHit, rayDistance, camadaDoChao)){
+			if(Vector3.Distance(transform.position, rayTopoHit.point) < 2.81f){
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+	IEnumerator MoveCamera(){
+		charController.height = estaAgachado ? alturaPadrao / 1.5f : alturaPadrao;
+		charController.center = new Vector3 (0,charController.height/2f,0);
+		camAltura = estaAgachado ? camPosPadrao.y / 1.5f : camPosPadrao.y;
+
+			while(Mathf.Abs(camAltura - fpsView.localPosition.y) > 0.01f){
+				fpsView.localPosition = Vector3.Lerp(fpsView.localPosition, new Vector3(camPosPadrao.x, camAltura, camPosPadrao.z),Time.deltaTime * 11f);
+			}
+
+		yield return null;
+				
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
